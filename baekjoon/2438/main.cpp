87e9fb9cd74797212b6258f7463412dca6eba03b
Sejeong Hong <dev.gwisket@gmail.com>
2023-01-03 18:00:54 +0900

    baekjoon/2438

---
+++ baekjoon/2438/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  unsigned N;
+
+  std::cin >> N;
+
+  for (unsigned i = 1; i <= N; i ++) {
+    for (unsigned j = 0; j < i; j ++) {
+      std::cout << "*";
+    }
+
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
+
